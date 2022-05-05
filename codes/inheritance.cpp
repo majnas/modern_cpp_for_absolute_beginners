32e5fb2c282de94995fe59f4be85382f2f683b13
Majid Nasiri <majid.nasiri@ehan.tech>
2022-05-05 19:49:35 +1000

    inheritance

--- codes/inheritance.cpp
+++ codes/inheritance.cpp
@@ -3,20 +3,38 @@
 class MyBaseClass
 {
     public:
-        char c;
-        int x;
-}
+        int publicVar;
+    
+    protected:
+        int protectedVar;
+
+    private:
+        int privateVar;
+};
 
 class MyDerivedClsss : public MyBaseClass
 {
-    // c and x also accessible here
-}
+    public:
+    MyDerivedClsss()
+    {
+        // publicVar is accessible here
+        this->publicVar = 123;
+        
+        // protectedVar is accessible here
+        this->protectedVar = 123;
+
+        // privateVar is NOT accessible here
+        this->privateVar = 123; /*ERROR*/
+    }
+};
 
 int main()
 {
     MyDerivedClsss obj;
-    obj.c = 'A';
-    obj.x = 123;
+
+    obj.publicVar = 123;
+    obj.protectedVar = 123;     /* Error: protectedVar is protected within this context  */
+    obj.privateVar = 123;       /* Error: privateVar is private within this context  */
 
     return 0;
 }
