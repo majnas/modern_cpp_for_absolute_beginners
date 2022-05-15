a9731a98270b4d37fc051821d1cf6be750ce8367
Majid Nasiri <majid.nasiri@ehan.tech>
2022-05-15 14:11:05 +1000

    update namespace

--- codes/namespace.cpp
+++ codes/namespace.cpp
@@ -1,25 +1,41 @@
 #include <iostream>
 
+//!< define namsespace NS1
 namespace NS1
 {
     int x = 1;
 }
 
+//!< define namsespace NS2
 namespace NS2
 {
     int x = 2;
+    namespace NS2NESTED
+    {
+        int x = 3;
+    }
 }
 
 int main()
 {
-    int x = 3;
+    //!< local variable
+    int x = 4;
 
-    // print x in NS1 namespace
-    std::cout << "NS1::x: " << NS1::x << std::endl;
+    //!< print x in NS1, NS2, NS2NESTED and local
+    std::cout << "NS1::x: " << NS1::x << std::endl; //!< Output >> NS1::x: 1 */
+    std::cout << "NS2::x: " << NS2::x << std::endl; //!< Output >> NS2::x: 2 */
+    std::cout << "NS2::NS2NESTED::x: " << NS2::NS2NESTED::x << std::endl; //!< Output >> NS2::NS2NESTED::x: 3 */
+    std::cout << "x: " << x << std::endl; //!< Output >> x:4 */
 
-    // print x in NS2 namespace
-    std::cout << "NS2::x: " << NS2::x << std::endl;
+    //!< Assign new value to variables
+    NS1::x = 11;
+    NS2::x = 22;
+    NS2::NS2NESTED::x = 33;
+    x = 44;
 
-    // print local variable x
-    std::cout << "x: " << x << std::endl;
+    //!< print x in NS1, NS2, NS2NESTED and local
+    std::cout << "NS1::x: " << NS1::x << std::endl; //!< Output >> NS1::x: 11 */
+    std::cout << "NS2::x: " << NS2::x << std::endl; //!< Output >> NS2::x: 22 */
+    std::cout << "NS2::NS2NESTED::x: " << NS2::NS2NESTED::x << std::endl; //!< Output >> NS2::NS2NESTED::x: 33 */
+    std::cout << "x: " << x << std::endl; //!< Output >> x:44 */
 }
